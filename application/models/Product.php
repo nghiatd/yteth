ca7cb2533ffa6c0834b1b19c6132abc2514d80b9
tdn <tdn@gmail.com>
2014-10-18 23:49:19 +0700

    add 2 models Bug and Product

---
+++ application/models/Product.php
@@ -0,0 +1,33 @@
+<?php
+// src/Product.php
+/**
+ * @Entity @Table(name="products")
+ */
+class Default_Model_Product
+{
+    /**
+     * @Id @Column(type="integer") @GeneratedValue
+     * @var int
+     */
+    protected $id;
+    /**
+     * @Column(type="string")
+     * @var String
+     */
+    protected $name;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+}
